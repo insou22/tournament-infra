0182a5a01db07bfe6c543a9462a3737a27b7be9e
HamishWHC <20359560+HamishWHC@users.noreply.github.com>
2021-06-21 22:31:16 +1000

    Removes games_incomplete, changes timestamps.

--- migrations/2021-06-21-120329_create_basics/up.sql
+++ migrations/2021-06-21-120329_create_basics/up.sql
@@ -4,25 +4,13 @@ CREATE TABLE users (
     display_name VARCHAR(32)
 );
 
-CREATE TABLE games_complete (
+CREATE TABLE games (
     id INTEGER NOT NULL,
     user_id INTEGER NOT NULL,
     points INTEGER,
     elo_change INTEGER,
-    created_at INTEGER,
+    created_at INTEGER NOT NULL,
     completed_at INTEGER,
-    tournament_id INTEGER NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users (id),
-    UNIQUE (game_id, user_id),
-    PRIMARY KEY (id, user_id)
-);
-
-CREATE TABLE games_incomplete (
-    id INTEGER NOT NULL,
-    user_id INTEGER NOT NULL,
-    points INTEGER,
-    elo_change INTEGER,
-    created_at INTEGER,
     tournament_id INTEGER NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users (id),
     UNIQUE (game_id, user_id),
@@ -34,12 +22,14 @@ CREATE TABLE turns (
     game_id INTEGER NOT NULL,
     turn_number INTEGER NOT NULL,
     player_id INTEGER NOT NULL,
-    created_at INTEGER,
+    created_at INTEGER NOT NULL,
+    time_taken INTEGER,
+    timed_out BOOLEAN,
     legal BOOLEAN,
     stdout TEXT,
     stderr TEXT,
     stdin TEXT,
-    FOREIGN KEY (game_id) REFERENCES games (id), -- FIXME: What does this refer to?
+    FOREIGN KEY (game_id) REFERENCES games (id),
     FOREIGN KEY (player_id) REFERENCES games (user_id),
     UNIQUE (game_id, turn_number)
 );
